688083d617e69c68427c642cbfbbb8b32530fae7
Kagome Miller <kagomemiller@gmail.com>
2021-02-14 18:39:45 -0600

    Add files via upload

---
+++ numV.py
@@ -0,0 +1,11 @@
+#print how many vowels occure in string
+
+s = 'azcbobobegghakl'
+numV = 0
+
+for char in s:
+    if char == 'o' or char == 'e' or char == 'i' or char == 'a' or char == 'u':
+        numV += 1
+        # print(char)
+
+print('numV is: ' + str(numV))
